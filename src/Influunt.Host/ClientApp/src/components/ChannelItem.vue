4e5089ff125d75e8573c9e1f17a415515a4597c2
Klabukov Erik <AMECT@ya.ru>
2025-06-03 23:43:40 +0500

    UI fixes (#17)
    * Fix channel items icon position
    * Add approve msgBox for remove channel and remove from favorites actions

--- src/Influunt.Host/ClientApp/src/components/ChannelItem.vue
+++ src/Influunt.Host/ClientApp/src/components/ChannelItem.vue
@@ -7,8 +7,8 @@
     <span style="width: 85%; line-height: 32px">{{ name }}</span>
     <div>
       <b-button variant="outline-warning" class="but" v-on:click="Edit"
-        ><b-icon-pencil icon="pencil" class="mr-2"
-      /></b-button>
+        ><b-icon-pencil icon="pencil" />
+      </b-button>
       <b-button
         variant="outline-warning"
         class="but"
@@ -58,11 +58,28 @@ export default {
       this.channel.hidden = !this.channel.hidden;
       InfluuntApi.UpdateChannel(this.channel);
     },
-    RemoveChannel: function () {
-      var self = this;
+    RemoveChannel: async function () {
+      var confirmResult = await this.$bvModal.msgBoxConfirm(
+        "Are you sure you want to delete this channel?",
+        {
+          title: "Confirm deletion",
+          size: "sm",
+          buttonSize: "sm",
+          okVariant: "danger",
+          okTitle: "Remove",
+          cancelTitle: "Cancel",
+          hideHeaderClose: false,
+          centered: true,
+          headerClass: "p-2 border-bottom-0",
+          footerClass: "p-2 border-top-0",
+          contentClass: "bg-dark text-light",
+        }
+      );
+      if (!confirmResult) return;
+      
+      const self = this;
+      // eslint-disable-next-line
       InfluuntApi.RemoveChannel(self.id, function (r) {
-        // eslint-disable-next-line
-        var rr = r;
         self.$forceUpdate();
         self.deleted = true;
       });
@@ -80,5 +97,9 @@ export default {
 }
 .list-group-item:hover {
   background-color: #333 !important;
+}
+.but svg {
+  position: relative;
+  top: -2px;
 }
 </style>
